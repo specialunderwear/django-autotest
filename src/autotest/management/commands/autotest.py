e2e49c8aa1a991602f9daeac639ea2ead8fe66fc
Lars van de Kerkhof <lars@permanentmarkers.nl>
2011-08-24 15:00:02 +0200

    Run tests every time a file was edited, but only run the test suite for the package the file was in.

--- src/autotest/management/commands/autotest.py
+++ src/autotest/management/commands/autotest.py
@@ -5,7 +5,7 @@ from django.core.management.base import BaseCommand
 from os import chdir
 from django.conf import settings
 from subprocess import Popen, PIPE 
-from os.path import split, dirname, join, abspath 
+from os.path import split, dirname, join, abspath, isfile
 import os.path
 import re
 
@@ -15,61 +15,60 @@ AUTOTEST_PATH = abspath(join(CURRENT_PATH, '..'))
 
 def absolute_path(path):
 
-	return os.path.abspath(os.path.normpath(path))
+    return os.path.abspath(os.path.normpath(path))
 
 
 class LoggingEventHandler(FileSystemEventHandler):
 
 
-	def run_test_suite(self):
+    def run_test_suite(self, app_name):
 
-		chdir(settings.PROJECT_ROOT)
-		result = Popen(['./manage.py', 'autotestrunner'], stdout=PIPE, stderr=PIPE, close_fds=True).communicate() 
+        chdir(settings.PROJECT_ROOT)
+        result = Popen(['./manage.py', 'autotestrunner', app_name], stdout=PIPE, stderr=PIPE, close_fds=True).communicate() 
 
-		title = ''
-		content = ''
-		for line in result:
-			split_lines = line.split('\n')
-			for l in split_lines:
-				if l.startswith('Ran'):
-					title = l
-				if l.startswith('OK') or l.startswith('FAIL'):
-					content = l
+        title = ''
+        content = ''
+        for line in result:
+            split_lines = line.split('\n')
+            for l in split_lines:
+                if l.startswith('Ran'):
+                    title = l
+                if l.startswith('OK') or l.startswith('FAIL'):
+                    content = l
 
-		chdir(AUTOTEST_PATH)
-		os.system('chmod u+rwx notify.sh')
-		os.system('./notify.sh "{0}" "{1}" '.format(title, content))
-		
+        chdir(AUTOTEST_PATH)
+        os.system('./notify.sh "{0}" "{1}" '.format(title, content))
+        
 
-	def on_modified(self, event):
-		super(LoggingEventHandler, self).on_modified(event)
-		path_to_file, filename = split(event.src_path)
-		valid_test = re.compile('(?:^|[\\b_\\.-])[Tt]ests?.py$') # extended from nose
-
-		if valid_test.search(filename):
-			self.run_test_suite()
-	
+    def on_modified(self, event):
+        super(LoggingEventHandler, self).on_modified(event)
+        if isfile(event.src_path):
+            for app in settings.INSTALLED_APPS:
+                if app in event.src_path:
+                    self.run_test_suite(app)
+                    break
+    
 
 class Command(BaseCommand):
-	option_list = BaseCommand.option_list + ()
-	help = 'Runs the test suite when a tests file is saved'
-	args = '[appname ...]'
+    option_list = BaseCommand.option_list + ()
+    help = 'Runs the test suite when a tests file is saved'
+    args = '[appname ...]'
 
-	requires_model_validation = False
+    requires_model_validation = False
 
-	def handle(self, *args, **options):
-		#handler = AutotestEventHandler()
-		handler = LoggingEventHandler()
+    def handle(self, *args, **options):
+        #handler = AutotestEventHandler()
+        handler = LoggingEventHandler()
 
-		app_path = absolute_path(settings.PROJECT_ROOT)
+        app_path = absolute_path(settings.PROJECT_ROOT)
 
-		observer = Observer()
-		observer.schedule(handler, path=app_path, recursive=True)
-		observer.start()
-		try:
-			while True:
-				time.sleep(1)
-		except KeyboardInterrupt:
-			observer.stop()
-		observer.join()
+        observer = Observer()
+        observer.schedule(handler, path=app_path, recursive=True)
+        observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
 
--- src/autotest/management/commands/autotestrunner.py
+++ src/autotest/management/commands/autotestrunner.py
@@ -3,17 +3,17 @@ from django.conf import settings
 
 
 def get_runner(settings):
-	runner = 'autotest.testrunner.AutotestSuiteRunner'
-	test_path = runner.split('.')
-	# Allow for Python 2.5 relative paths
-	if len(test_path) > 1:
-		test_module_name = '.'.join(test_path[:-1])
-	else:
-		test_module_name = '.'
-	test_module = __import__(test_module_name, {}, {}, test_path[-1])
-	test_runner = getattr(test_module, test_path[-1])
+    runner = 'autotest.testrunner.AutotestSuiteRunner'
+    test_path = runner.split('.')
+    # Allow for Python 2.5 relative paths
+    if len(test_path) > 1:
+        test_module_name = '.'.join(test_path[:-1])
+    else:
+        test_module_name = '.'
+    test_module = __import__(test_module_name, {}, {}, test_path[-1])
+    test_runner = getattr(test_module, test_path[-1])
 
-	return test_runner
+    return test_runner
 
 
 class Command(BaseCommand):
@@ -25,16 +25,16 @@ class Command(BaseCommand):
 
     def handle(self, *test_labels, **options):
       
-		TestRunner = get_runner(settings)
-		
-		if hasattr(TestRunner, 'func_name'):
-			import warnings
-			warnings.warn(
-					'Function-based test runners are deprecated. Test runners should be classes with a run_tests() method.',
-					DeprecationWarning
-					)
-			failures = TestRunner(verbosity=1, interactive=True)
-		else:
-			test_runner = TestRunner(verbosity=1, interactive=True, failfast=True)
-			failures = test_runner.run_tests(test_labels)
+        TestRunner = get_runner(settings)
+        
+        if hasattr(TestRunner, 'func_name'):
+            import warnings
+            warnings.warn(
+                    'Function-based test runners are deprecated. Test runners should be classes with a run_tests() method.',
+                    DeprecationWarning
+                    )
+            failures = TestRunner(verbosity=1, interactive=True)
+        else:
+            test_runner = TestRunner(verbosity=1, interactive=True, failfast=True)
+            failures = test_runner.run_tests(test_labels)
 
